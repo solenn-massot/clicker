cce5ce52fa28f00700d83aa532312bb0e43ebf65
[solenn] <solenn.massot@laplateforme.io>
2020-04-28 12:09:49 +0200

    //

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <script type="text/javascript" src="https://code.jquery.com/jquery-3.4.1.js"></script>
+    <script src="script.js"></script>
+    <link href="https://fonts.googleapis.com/css2?family=Manrope&display=swap" rel="stylesheet">
+    <link rel='stylesheet' href='style.css'>
+    <title>Accueil</title>
+</head>
+<body>
+    <header>
+        <span><h1>Bienvenue sur Animal clicker</h1>
+        Votre but ? Récolter le plus de clochettes possible pour devenir le.a plus riche de tous !</span>
+    </header>
+    <main>
+    </main>
+
+</body>
