8d59b5c19a49232885ed35f142eccbbb7e6b666e
[solenn] <solenn.massot@laplateforme.io>
2020-07-07 16:04:07 +0200

    modification des coûts et bonus des équipements

--- class/equipement.js
+++ class/equipement.js
@@ -1,11 +1,10 @@
 class Equipements {
-    constructor(slug, img, cost, bonus, compteur, characters, nb = 0) {
+    constructor(slug, img, cost, bonus, characters, nb = 0) {
         this.slug = slug;
         this.img = img;
         this.cost = cost;
         this.bonus = bonus;
         this.nb = nb;
-        this.compteur = compteur;
         this.characters = characters;
     }
 
@@ -17,31 +16,31 @@ class Equipements {
     updateEquipement(slug, nb) {
         if (slug === "pelle" && nb === 1) {
             this.img = "src/img/shovel_normal.png";
-            this.bonus = this.bonus + 2;
+            this.bonus = this.bonus * 2;
             this.cost = this.cost * 3;
         } else if (slug === "pelle" && nb === 2) {
             this.img = "src/img/pelle_dore.png";
-            this.bonus = this.bonus + 2;
+            this.bonus = this.bonus * 3;
             this.cost = this.cost * 5;
         }
         else if(slug === "canne" && nb === 1){
             this.img = "src/img/canne_normal.png";
-            this.bonus = this.bonus + 3;
+            this.bonus = this.bonus * 2;
             this.cost = this.cost * 5;
         }
         else if(slug === "canne" && nb === 2){
             this.img = "src/img/canne_dore.png";
-            this.bonus = this.bonus + 5;
+            this.bonus = this.bonus * 3;
             this.cost = this.cost * 7;
         }
         else if(slug === "filet" && nb === 1){
             this.img = "src/img/filet_normal.png";
-            this.bonus = this.bonus + 5;
+            this.bonus = this.bonus * 2;
             this.cost = this.cost * 4;
         }
         else if(slug === "filet" && nb === 2){
             this.img = "src/img/filet_dore.png";
-            this.bonus = this.bonus + 8;
+            this.bonus = this.bonus * 3;
             this.cost = this.cost * 8;
         }
     }
@@ -49,11 +48,11 @@ class Equipements {
 }
 
 
-var craft = new Equipements("craft", "src/img/craft.png", 100, 3, 3, "meli");
-var object = new Equipements("objet", "src/img/objet.png", 100, 3, 3, "melo");
-var shovel = new Equipements("pelle", "src/img/pelle_rudimentaire.png", 100, 3, 3, "thibou");
-var fish = new Equipements("canne", "src/img/canne_rudimentaire.png", 100, 3, 3, "pollux");
-var filet = new Equipements("filet", "src/img/filet_rudimentaire.png", 100, 3, 3, "djason");
-var navet = new Equipements("navet", "src/img/navet.png", 100, 3, 3, "daisy");
+var craft = new Equipements("craft", "src/img/craft.png", 400, 5, "meli");
+var object = new Equipements("objet", "src/img/objet.png", 700, 6, "melo");
+var shovel = new Equipements("pelle", "src/img/pelle_rudimentaire.png", 1200, 8, "thibou");
+var fish = new Equipements("canne", "src/img/canne_rudimentaire.png", 2000, 10, "pollux");
+var filet = new Equipements("filet", "src/img/filet_rudimentaire.png", 2500, 15, "djason");
+var navet = new Equipements("navet", "src/img/navet.png", 5000, 20, "daisy");
 
 var allequipement = [craft, object, shovel, fish, filet, navet];
